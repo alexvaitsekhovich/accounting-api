8c2e4274c9cc703b5f5ea26053ef33b684ed78c2
Alexander Vaitsekhovich <alex.vaitsekhovich@gmail.com>
2021-05-04 23:46:43 +0200

    Creation date added, MapStruct mapping refactored

--- src/test/java/com/alexvait/accountingapi/usermanagement/controller/AdminControllerPermissionsTest.java
+++ src/test/java/com/alexvait/accountingapi/usermanagement/controller/AdminControllerPermissionsTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -63,38 +62,6 @@ class AdminControllerPermissionsTest {
     @DisplayName("Get all users, with normal user")
     void listUsersByUser() throws Exception {
         mockMvc.perform(get(AdminController.BASE_URL + "/user"))
-                .andExpect(status().isForbidden());
-    }
-
-
-    @Test
-    @DisplayName("Delete a user, without authentication")
-    void deleteUserNotAuth() throws Exception {
-        mockMvc.perform(delete(AdminController.BASE_URL + "/user/" + testUserPublicId))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    @WithUserDetails("users-admin@api.com")
-    @DisplayName("Delete a user, with users admin")
-    void deleteUserByUsersAdmin() throws Exception {
-        mockMvc.perform(delete(AdminController.BASE_URL + "/user/" + testUserPublicId))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @WithUserDetails("super-admin@api.com")
-    @DisplayName("Delete a user, with super admin")
-    void deleteUserBySuperAdmin() throws Exception {
-        mockMvc.perform(delete(AdminController.BASE_URL + "/user/" + testUserPublicId))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @WithUserDetails("john.doe1@api.com")
-    @DisplayName("Delete a user, with normal user")
-    void deleteUserByUser() throws Exception {
-        mockMvc.perform(delete(AdminController.BASE_URL + "/user/" + testUserPublicId))
                 .andExpect(status().isForbidden());
     }
 }
